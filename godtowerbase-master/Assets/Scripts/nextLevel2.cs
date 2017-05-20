f4bf63770e3884dcad8b2e07b2b24e694d81b5d9
cuongnve04982 <cuongnvs04982@gmail.com>
2017-05-20 21:51:40 +0700

    Done Scene2

---
+++ godtowerbase-master/Assets/Scripts/nextLevel2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextLevel2 : MonoBehaviour {
+
+	public void NextScene(){
+		TKSceneManager.ChangeScene ("Level2");
+	}
+}
