6a9c24b843a0b47f8cf938909368a1dc9f1a8273
cuongnve04982 <cuongnvs04982@gmail.com>
2017-05-24 19:27:00 +0700

    Done!

--- godtowerbase-master/Assets/Scripts/MoveImage.cs
+++ godtowerbase-master/Assets/Scripts/MoveImage.cs
@@ -1,19 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
 
 public class MoveImage : MonoBehaviour {
 
 	private bool mouseDown = false;
+	public Transform imageTransform;
 
-
+	Vector3 imagePos ;
+	Vector3 mousePos;;
 	// Use this for initialization
 	void Start () {
-		mouseDownDown = false;
+		
 	}
-	
+
+	public void ChangePosOfImage(){
+		imagePos = imageTransform.position;
+		mousePos = Input.mousePosition;
+
+
+	}
 	// Update is called once per frame
 	void Update () {
 		
 	}
 }
+
+
+
