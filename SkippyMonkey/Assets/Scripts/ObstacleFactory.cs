0840a213e178b25d5ec292a7a7dac69114e51379
cuongnve04982 <cuongnvs04982@gmail.com>
2017-06-15 18:29:14 +0700

    Done Score Label

---
+++ SkippyMonkey/Assets/Scripts/ObstacleFactory.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleFactory : MonoBehaviour {
+    public GameObject obstaclePrefab;
+    public float startY;
+    public float platformSpacing;
+    public float platformGapHalfWidth;
+
+    private int currentPlatformIndex = 0;
+    private float spawnHalfWidth;
+
+    private List<GameObject> platformPool = new List<GameObject>();
+
+    private void Start()
+    {
+        spawnHalfWidth = Camera.main.aspect * Camera.main.orthographicSize - platformGapHalfWidth;
+
+        _CreateNewPlatformIfNeeded();
+    }
+
+    private GameObject _GetNewPlatform()
+    {
+        foreach(GameObject platform in platformPool)
+        {
+            if (!platform.activeInHierarchy)
+            {
+                return platform;
+            }
+        }
+
+        GameObject newPlatform = Instantiate(
+            obstaclePrefab,
+            Vector3.zero,
+            Quaternion.identity
+        );
+
+        platformPool.Add(newPlatform);
+
+        return newPlatform;
+    }
+
+    private void _CreateNewPlatformIfNeeded()
+    {
+        while (currentPlatformIndex * platformSpacing + startY < Camera.main.transform.position.y + Camera.main.orthographicSize)
+        {
+            GameObject newPlatform = _GetNewPlatform();
+            newPlatform.transform.position = new Vector3(Random.Range(-spawnHalfWidth, spawnHalfWidth), currentPlatformIndex * platformSpacing + startY, 0);
+            newPlatform.SetActive(true);
+            currentPlatformIndex++;
+        }
+    }
+
+    private void Update()
+    {
+        _CreateNewPlatformIfNeeded();
+
+        foreach(GameObject platform in platformPool)
+        {
+            if(platform.transform.position.y < Camera.main.transform.position.y - Camera.main.orthographicSize)
+            {
+                platform.SetActive(false);
+            }
+        }
+    }
+}
